025479c8e94eee024a82de2f18adc15f4e5ec2ed
lowfront <fronterior@hotmail.com>
2021-09-12 12:49:19 +0900

    update

--- src/napi-xinput.cc
+++ src/napi-xinput.cc
@@ -3,29 +3,38 @@
 #include <Xinput.h>
 #include <napi.h>
 
-#include <chrono>
-#include <thread>
-
 DWORD dwResult;
 
-std::thread t1([] {
-    while (1) {}
-});
-
-Napi::Boolean GetState(const Napi::CallbackInfo& info)
+Napi::Object GetState(const Napi::CallbackInfo& info)
 {
   Napi::Env env = info.Env();
+  Napi::Object result = Napi::Object::New(env);
 
   if (info.Length() < 1) {
     Napi::TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
-    return Napi::Boolean::New(env, false);
+    return result;
   }
   int i = info[0].ToNumber().Int32Value();
   XINPUT_STATE state;
   ZeroMemory(&state, sizeof(XINPUT_STATE));
   dwResult = XInputGetState(i, &state);
 
-  return Napi::Boolean::New(env, dwResult == ERROR_SUCCESS);
+  if (dwResult == ERROR_SUCCESS)
+  {
+    result.Set("dwPacketNumber", state.dwPacketNumber);
+    result.Set("Gamepad.wButtons", state.Gamepad.wButtons);
+    result.Set("Gamepad.bLeftTrigger", state.Gamepad.bLeftTrigger);
+    result.Set("Gamepad.bRightTrigger", state.Gamepad.bRightTrigger);
+    result.Set("Gamepad.sThumbLX", state.Gamepad.sThumbLX);
+    result.Set("Gamepad.sThumbLY", state.Gamepad.sThumbLY);
+    result.Set("Gamepad.sThumbRX", state.Gamepad.sThumbRX);
+    result.Set("Gamepad.sThumbRY", state.Gamepad.sThumbRY);
+    return result;
+  }
+  else 
+  {
+    return result;
+  }
 }
 Napi::Boolean SetState(const Napi::CallbackInfo& info)
 {
@@ -69,18 +78,114 @@ Napi::Object GetBatteryInformation(const Napi::CallbackInfo& info)
   int type = info[0].ToNumber().Int32Value(); // 0 - BATTERY_DEVTYPE_GAMEPAD, 1 - BATTERY_DEVTYPE_HEADSET
   
   XInputGetBatteryInformation(i, type, &pBatteryInformation);
-
-  result.Set("type", pBatteryInformation.BatteryType);
-  result.Set("level", pBatteryInformation.BatteryLevel);
+  
+  result.Set("BatteryType", pBatteryInformation.BatteryType);
+  result.Set("BatteryLevel", pBatteryInformation.BatteryLevel);
 
   return result;
 }
+
+Napi::Object GetCapabilities(const Napi::CallbackInfo& info)
+{
+  Napi::Env env = info.Env();
+  Napi::Object result = Napi::Object::New(env);
+
+  if (info.Length() < 1) {
+    Napi::TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
+    return result;
+  }
+  int i = info[0].ToNumber().Int32Value();
+
+  XINPUT_CAPABILITIES pCapabilities;
+  ZeroMemory(&pCapabilities, sizeof(XINPUT_CAPABILITIES));
+
+  XInputGetCapabilities(i, XINPUT_FLAG_GAMEPAD, &pCapabilities);
+
+  result.Set("Type", pCapabilities.Type);
+  result.Set("SubType", pCapabilities.SubType);
+  result.Set("Flags", pCapabilities.Flags);
+  result.Set("Gamepad.wButtons", uint16_t(pCapabilities.Gamepad.wButtons));
+  result.Set("Gamepad.bLeftTrigger", pCapabilities.Gamepad.bLeftTrigger);
+  result.Set("Gamepad.bRightTrigger", pCapabilities.Gamepad.bRightTrigger);
+  result.Set("Gamepad.sThumbLX", uint16_t(pCapabilities.Gamepad.sThumbLX));
+  result.Set("Gamepad.sThumbLY", uint16_t(pCapabilities.Gamepad.sThumbLY));
+  result.Set("Gamepad.sThumbRX", uint16_t(pCapabilities.Gamepad.sThumbRX));
+  result.Set("Gamepad.sThumbRY", uint16_t(pCapabilities.Gamepad.sThumbRY));
+  result.Set("Vibration.wLeftMotorSpeed", pCapabilities.Vibration.wLeftMotorSpeed);
+  result.Set("Vibration.wRightMotorSpeed", pCapabilities.Vibration.wRightMotorSpeed);
+
+  return result;
+}
+
+Napi::Object GetKeystroke(const Napi::CallbackInfo& info)
+{
+  Napi::Env env = info.Env();
+  Napi::Object result = Napi::Object::New(env);
+
+  if (info.Length() < 1) {
+    Napi::TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
+    return result;
+  }
+  int i = info[0].ToNumber().Int32Value();
+
+  PXINPUT_KEYSTROKE pKeystroke;
+  ZeroMemory(pKeystroke, sizeof(PXINPUT_KEYSTROKE));
+
+  XInputGetKeystroke(i, 0, pKeystroke);
+  result.Set("VirtualKey", uint16_t(pKeystroke->VirtualKey));
+  result.Set("Unicode", pKeystroke->Unicode);
+  result.Set("Flags", pKeystroke->Flags);
+  result.Set("UserIndex", pKeystroke->UserIndex);
+  result.Set("HidCode", pKeystroke->HidCode);
+  
+  return result;
+}
+
+// Napi::Object GetCapabilitiesEx(const Napi::CallbackInfo& info)
+// {
+//   Napi::Env env = info.Env();
+//   Napi::Object result = Napi::Object::New(env);
+
+//   struct XINPUT_CAPABILITIES_EX
+//   {
+//       XINPUT_CAPABILITIES Capabilities;
+//       WORD vendorId;
+//       WORD productId;
+//       WORD revisionId;
+//       DWORD a4;
+//   };
+
+//   typedef DWORD(_stdcall* _XInputGetCapabilitiesEx)(DWORD a1, DWORD dwUserIndex, DWORD dwFlags, XINPUT_CAPABILITIES_EX* pCapabilities);
+//   _XInputGetCapabilitiesEx XInputGetCapabilitiesEx;
+
+//   HMODULE moduleHandle = LoadLibrary(TEXT("XInput1_4.dll"));
+//   XInputGetCapabilitiesEx = (_XInputGetCapabilitiesEx)GetProcAddress(moduleHandle, (char*)108);
+  
+//   if (info.Length() < 1) {
+//     Napi::TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
+//     return result;
+//   }
+
+//   int i = info[0].ToNumber().Int32Value();
+
+//   XINPUT_CAPABILITIES_EX capsEx;
+//   XInputGetCapabilitiesEx(1, i, 0, &capsEx);
+
+//   result.Set("vendorId", capsEx.vendorId);
+//   result.Set("productId", capsEx.productId);
+//   result.Set("revisionId", capsEx.revisionId);
+//   result.Set("a4", capsEx.a4);
+
+//   return result;
+// }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports)
 {
   exports.Set(Napi::String::New(env, "GetState"), Napi::Function::New(env, GetState));
   exports.Set(Napi::String::New(env, "SetState"), Napi::Function::New(env, SetState));
   exports.Set(Napi::String::New(env, "GetBatteryInformation"), Napi::Function::New(env, GetBatteryInformation));
+  exports.Set(Napi::String::New(env, "GetCapabilities"), Napi::Function::New(env, GetCapabilities));
+  exports.Set(Napi::String::New(env, "GetKeystroke"), Napi::Function::New(env, GetKeystroke));
   return exports;
 }
 
