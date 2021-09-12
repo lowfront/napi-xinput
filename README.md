0adb18318e5fe5699d9f6972e5bf516a5d916e0b
lowfront <fronterior@hotmail.com>
2021-09-12 16:31:30 +0900

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+# Node XInput
+XInput wrapper for Node.js. It aims to implement xinput as similar as possible to the document, and plans to add utilities for Javascript in the future.
+
---
+++ test/run.js
@@ -0,0 +1,17 @@
+const {xinput, constants} = require('../lib/node-xinput');
+console.log(xinput);
+// console.log(xinput.GetState(0));
+// console.log(xinput.SetState(0, 65535, 65535));
+// console.log(xinput.GetBatteryInformation(0, 0));
+// console.log(xinput.GetCapabilities(0));
+// console.log(xinput.GetKeystroke(0));
+// console.log(1);
+// let i = 0;
+// setImmediate(function f() {
+//   xinput.SetState(0, i, i);
+//   i = (i + 10) % 65535;
+//   console.clear();
+//   console.log(i);
+//   setImmediate(f);
+// });
+// while (1) {}
